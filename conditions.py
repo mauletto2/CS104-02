cdd0905d7b2200a445b17fc06b361bb32097bcd6
mauletto2 <71794423+mauletto2@users.noreply.github.com>
2020-10-12 16:27:11 -0400

    Create conditions.py

---
+++ conditions.py
@@ -0,0 +1,11 @@
+temp = int(input("Please enter the current temperature")
+	   if temp > 90
+	   (print("Wear shorts"))
+	   else temp > 70
+	   (print("Short sleeves are fine"))
+	   if temp > 50
+	   (print("Wear a jacket"))
+	   else temp > 32
+	   (print("Wear a heavy coat"))
+	   if temp < 32
+     (print("Stay inside"))
