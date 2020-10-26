8c45a6f864b875af7a5a8414cb17a1c259c6ccb9
mauletto2 <71794423+mauletto2@users.noreply.github.com>
2020-10-26 17:41:09 -0400

    Update ForLoop.py

--- ForLoop.py
+++ ForLoop.py
@@ -1,6 +1,8 @@
 total=0
-for x in range (0,10):
-	x=1
-	print (total)
-	total=total + x
-	x=x + 1
+for x in range (11):
+    if x==10:
+        print(total)
+        break
+    else:
+        total +=x
+        x+= 1
